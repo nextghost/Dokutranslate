05c29a09d79dc124b8d6a0468d24c1c1ca78fe20
Martin Doucha <next_ghost@quick.cz>
2012-06-18 20:42:21 +0200

    Add Czech translation

---
+++ lang/cs/lang.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Czech language file for dokutranslate plugin
+ *
+ * @author Martin Doucha <next_ghost@quick.cz>
+ */
+
+$lang['translate_begin'] = 'Zahájit překlad (nelze vrátit zpět)';
+$lang['admin_dokutranslate'] = 'Správa moderátorských skupin pluginu Dokutranslate';
+$lang['current'] = 'Současné přiřazení moderátorských skupin';
+$lang['modgroup'] = 'Moderátorská skupina:';
+$lang['where'] = 'Jmenný prostor';
+$lang['who'] = 'Moderátorská skupina';
+$lang['delsel'] = 'Smazat vybrané';
+$lang['trans_message'] = 'Komentář:';
+$lang['trans_wrong'] = 'Chybný překlad';
+$lang['trans_rephrase'] = 'Správný, ale potřebuje přeformulovat';
+$lang['trans_accepted'] = 'Vynikající';
+$lang['trans_quality'] = 'Kvalita překladu:';
+$lang['trans_incomplete'] = 'Tento odstavec je neúplný';
+$lang['add_review'] = 'Uložit hodnocení';
+$lang['rend_incomplete'] = 'Neúplný';
+$lang['review_header'] = 'Hodnocení překladu';
+
+//Setup VIM: ex: et ts=4 :
